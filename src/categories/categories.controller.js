42fcd69f9aa813b9c5cf4956b11c543735e33530
Stephen James Broida <87044792+SJBroida@users.noreply.github.com>
2021-10-17 16:15:31 -0400

    Updated files
    Uploaded modified files after following provided instructions to practice and study with.

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,13 +1,38 @@
+const categoriesService = require("./categories.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+/*  
+***Chaining (.then) Method***
+
+function list(req, res, next) {
+  categoriesService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
+}
+*/
+
+/*
+***Async/Await Method
+async function list(req, res) {
+  const data = await categoriesService.list();
+  res.json({ data });
+}
+*/
+
+/*
+***Async/Await Method with try/catch error handling
+*/
 async function list(req, res, next) {
-  res.json({
-    data: [
-      { category_name: "category 1" },
-      { category_name: "category 2" },
-      { category_name: "category 3" },
-    ],
-  });
+  try {
+    const data = await categoriesService.list();
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
-  list: [list],
+  list: asyncErrorBoundary(list),
 };
